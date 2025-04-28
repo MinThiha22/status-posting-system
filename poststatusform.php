4d47365ba544f76a2023205d494d357177f2f61c
Min Thiha Ko Ko (Mike) <minthihakoko@gmail.com>
2025-04-28 18:28:01 +1200

    Initialise assignment 1 and finish index page

---
+++ poststatusform.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Post Status</title>
+</head>
+<body>
+  <p class="mt-3 text-center font-Sansation text-2xl font-bold">Status Posting System</p>
+  
+</body>
+</html>
